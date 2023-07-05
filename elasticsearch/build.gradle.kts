55890f3c0f5d978f7f85729808c4f25187f4f9c8
Dean Wette <wetted@unityfoundation.io>
2023-07-05 10:05:54 -0500

    Enable binary compatibility check (#412)
    * Enable binary compatibility check
    
    * baselineVersion not needed

--- elasticsearch/build.gradle.kts
+++ elasticsearch/build.gradle.kts
@@ -30,9 +30,3 @@ tasks {
         systemProperty("elasticsearch.version", libs.versions.managed.elasticsearch.get())
     }
 }
-
-micronautBuild {
-    binaryCompatibility {
-        enabled.set(false)
-    }
-}
