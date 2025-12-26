ddb796adb13b508f7781858f1b40bbc0521a33aa
issam liqali <liqaliissam@gmail.com>
2025-12-26 09:34:48 +0100

    add testcontainers

--- test-suite-java/src/test/java/micronaut/example/ElasticSearchTest.java
+++ test-suite-java/src/test/java/micronaut/example/ElasticSearchTest.java
@@ -1,16 +1,28 @@
 package micronaut.example;
 
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.elasticsearch.ElasticSearch;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.micronaut.test.support.TestPropertyProvider;
 import micronaut.example.service.Movie;
 import micronaut.example.service.MovieService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+import java.util.Map;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.awaitility.Awaitility.await;
 
 @MicronautTest
-class ElasticSearchTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ElasticSearchTest implements TestPropertyProvider {
+
+    @Override
+    public @NonNull Map<String, String> getProperties() {
+        return ElasticSearch.getProperties();
+    }
+
     @Test
     void testElasticSearch(MovieService movieService) {
         String title = "Die Hard";
