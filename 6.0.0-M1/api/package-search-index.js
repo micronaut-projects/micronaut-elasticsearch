ddde6fd5029d316045273bf7e0268cf9d96254b1
sdelamo <sdelamo@users.noreply.github.com>
2025-12-30 20:57:02 +0000

    Deploying to gh-pages - 20:57:02

---
+++ 6.0.0-M1/api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"io.micronaut.elasticsearch"},{"l":"io.micronaut.elasticsearch.conditon"},{"l":"io.micronaut.elasticsearch.convert"},{"l":"io.micronaut.elasticsearch.health"},{"l":"io.micronaut.elasticsearch.info"}];updateSearchResults();
